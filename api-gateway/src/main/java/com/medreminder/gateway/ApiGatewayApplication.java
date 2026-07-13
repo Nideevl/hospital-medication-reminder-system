eca8062777f4a2482959e41e35fd9178327359f5
Deepak Singh Bungla <nideevl4574@gmail.com>
2026-07-13 12:02:36 +0530

    Basic setup and config done

---
+++ api-gateway/src/main/java/com/medreminder/gateway/ApiGatewayApplication.java
@@ -0,0 +1,31 @@
+﻿package com.medreminder.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer() {
+        return factory -> factory.setPort(8080);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+            .route("example_route", r -> r
+                .path("/api/**")
+                .uri("http://localhost:8081"))
+            .build();
+    }
+}
